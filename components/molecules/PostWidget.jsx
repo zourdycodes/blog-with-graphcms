4e9b1cd24742307e9360c2705015cf577f20eab6
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-11-13 13:25:43 +0000

    ✨ building post widgets feature
    checked

--- components/molecules/PostWidget.jsx
+++ components/molecules/PostWidget.jsx
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import moment from 'moment';
+import Link from 'next/link';
 
-export const PostWidget = () => {
+import {
+  getRecentPosts,
+  getSimilarPosts,
+} from '../../services/contentManagement';
+
+export const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      );
+    } else {
+      getRecentPosts().then((result) => setRelatedPosts(result));
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [slug]);
+
   return (
-    <div>
-      <h1>Post Widgets</h1>
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        {slug ? 'Related Posts' : ' Recent Posts'}
+      </h3>
+
+      {relatedPosts.map((post, index) => {
+        return (
+          <div key={index} className="flex items-center w-full mb-4">
+            <div className="w-16 flex-none">
+              <Image
+                alt={post.title}
+                height="60px"
+                width="60px"
+                unoptimized
+                className="align-middle rounded-full"
+                src={post.featuredImage.url}
+              />
+            </div>
+
+            <div className="flex-grow ml-4">
+              <p className="text-gray-500 font-xs">
+                {moment(post.createdAt).format('MMM DD, YYYY')}
+              </p>
+              <Link
+                href={`/post/${post.slug}`}
+                passHref
+                className="text-md"
+                key={index}
+              >
+                {post.title}
+              </Link>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- services/contentManagement.js
+++ services/contentManagement.js
@@ -1,7 +1,7 @@
 import { request, gql } from 'graphql-request';
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
-console.log(graphqlAPI);
+
 export const getPosts = async () => {
   const query = gql`
     query MyQuery {
